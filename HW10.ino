8bf21b65a65814229465711e214df6dfe33d48ee
Ayushi Shah <as17726@nyu.edu>
2023-11-14 20:41:36 -0500

    complete

--- HW10.ino
+++ HW10.ino
@@ -1,16 +1,42 @@
 unsigned long WallSeconds; 
 void setup() {
   // put your setup code here, to run once:
-    Serial.begin(9600);
-    pinMode(2, OUTPUT); 
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT); 
+    pinMode(4, OUTPUT); 
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT); 
+    pinMode(7, OUTPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
-  WallSeconds = currentMillis / 1000;
-  digitalWrite(2, HIGH); 
-  delay(100); 
-  digitalWrite(2, LOW); 
-  delay(100); 
+  WallSeconds = (currentMillis / 1000) % 61;
+  int p2v = (millis()/1000) % 2; 
+  int p3v = (millis()/5000) % 2;
+  int p4v = (millis()/10000) % 2;  
+  digitalWrite(2, p2v); 
+  digitalWrite(3, p3v); 
+  digitalWrite(4, p4v); 
+
+  if(WallSeconds>0 && WallSeconds<20)
+  {
+    digitalWrite(5, 1);
+    digitalWrite(6, 0);  
+    digitalWrite(7, 0); 
+  }
+   else if(WallSeconds>20 && WallSeconds<40)
+  {
+    digitalWrite(5, 0);
+    digitalWrite(6, 1);  
+    digitalWrite(7, 0); 
+  }
+ else 
+  {
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);  
+    digitalWrite(7, 1); 
+  }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HW10
  
+ I depicted the time in 2 different ways. 
+ Firstly, I used 3 LED's to show the passage of different lengths of time. 
+ The first LED blinks every second while the second LED should blink every 5 seconds and the third every 10 seconds. 
+ Secondly, I used 3 LED's to depict 3 brackets of time: 0-20 seconds, 20-40 seconds, and 40-60 seconds using 3 LED's. 
+ So the first LED would turn on for 0-20 seconds, second LED would turn on for 20-40 seconds, and third LED would turn on for 40-60 seconds. 
+ Based on the 2 different concepts, I thought that time could be deciphered based on which bracket of time we are in, and subsequently how many 1,5 or 10 seconds have passed within that bracket of time. 
+
+ ![G92q9G.jpg](https://imgpile.com/images/G92q9G.jpg)
+![G922za.jpg](https://imgpile.com/images/G922za.jpg) 
